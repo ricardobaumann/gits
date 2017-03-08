532f96d32f397f60bebbd2e085411f5cb848a976
ricardobaumann <ricardo.luis.baumann@gmail.com>
2017-03-08 16:13:08 +0100

    ptp

--- ptp.sh
+++ ptp.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 current_branch=`git rev-parse --abbrev-ref HEAD`
 git pull origin ${current_branch}
-git pull origin master && mvn clean package install && git push origin ${current_branch}
+set -e
+git pull origin master
+mvn clean package install
+git add -A . && git commit
+git push origin ${current_branch}
+hub pull-request -h "${current_branch}"
